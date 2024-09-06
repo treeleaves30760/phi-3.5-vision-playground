dafd64509a02842a25aea5a24c9fc6aeb185bf28
treeleaves30760 <treeleaves30760@gmail.com>
2024-09-07 00:00:22 +0800

    Refactor interact_script.py for improved model loading and inference

--- interact_script.py
+++ interact_script.py
@@ -1,20 +1,21 @@
 from PIL import Image
 import requests
-from transformers import AutoModelForCausalLM, AutoProcessor
-import torch
+from transformers import AutoModelForCausalLM
+from transformers import AutoProcessor
 
 model_id = "microsoft/Phi-3.5-vision-instruct"
 
-# Load the model with 8-bit quantization
+# Note: set _attn_implementation='eager' if you don't have flash_attn installed
+#       or set _attn_implementation='flash_attention_2' if you have flash_attn installed
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    device_map="auto",
+    device_map="cuda",
     trust_remote_code=True,
-    torch_dtype=torch.bfloat16,
-    load_in_4bit=True,
+    torch_dtype="auto",
     _attn_implementation='eager'
 )
 
+# for best performance, use num_crops=4 for multi-frame, num_crops=16 for single-frame.
 processor = AutoProcessor.from_pretrained(model_id,
                                           trust_remote_code=True,
                                           num_crops=4
@@ -47,11 +48,10 @@ generation_args = {
     "do_sample": False,
 }
 
-with torch.inference_mode():
-    generate_ids = model.generate(**inputs,
-                                  eos_token_id=processor.tokenizer.eos_token_id,
-                                  **generation_args
-                                  )
+generate_ids = model.generate(**inputs,
+                              eos_token_id=processor.tokenizer.eos_token_id,
+                              **generation_args
+                              )
 
 # remove input tokens
 generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
